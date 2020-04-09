a476b997a06323aca3d35cf80946ca38ab532f80
Edwin Chow <echow6@gmail.com>
2020-04-08 22:33:26 -0500

    use fe build

---
+++ build/precache-manifest.9e2a9d981578cbbbbccc51f0a1ec3bfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b50426a4f7ba35b1844a480916f675",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2004843a3738d3d920ac",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c2b393b6f72386ed370",
+    "url": "/static/js/2.cc2c1339.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc2c1339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2004843a3738d3d920ac",
+    "url": "/static/js/main.8449dbb0.chunk.js"
+  },
+  {
+    "revision": "3d09fc7fbfc0228634fe",
+    "url": "/static/js/runtime-main.7266208d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
