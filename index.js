b92b3d0e93a484c8cfa9802c0e983a513bd6f4e8
Edwin Chow <echow6@gmail.com>
2020-04-09 19:42:15 -0500

    be able to create, update, delete, handle errors

--- index.js
+++ index.js
@@ -9,24 +9,39 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('build'));
 
+const errorHandler = (error, request, response, next) => {
+    console.log(JSON.stringify(error.reason));
+
+    if (error.name === 'CastError' && error.path === '_id') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error);
+}
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>');
 });
 
-app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes);
-    });
+app.get('/api/notes', (request, response, next) => {
+    Note.find({})
+        .then(notes => {
+            response.json(notes);
+        })
+        .catch(next);
 });
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
         .then(note => {
             response.json(note);
-        });
+        })
+        .catch(next);
 });
 
-app.post('/api/notes', (request, response) => {
+app.use(errorHandler);
+
+app.post('/api/notes', (request, response, next) => {
     const body = request.body;
 
     if (!body.content) {
@@ -44,14 +59,31 @@ app.post('/api/notes', (request, response) => {
     note.save()
         .then(savedNote => {
             response.json(savedNote);
-        });
+        })
+        .catch(next);;
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body;
 
-    response.status(204).end();
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(updatedNote => {
+            response.json(updatedNote);
+        })
+        .catch(next);
+
+});
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(() => response.status(204).end())
+        .catch(next);
+
 });
 
 const PORT = process.env.PORT;
